301585df2c5e72c3822535c0e6711fc842b9c449
realnihal <puramnihal@gmail.com>
2022-06-04 20:58:52 +0530

    backend done

---
+++ lib/Home Screens/mainpage2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainPage2 extends StatelessWidget {
+  const MainPage2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('output'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Home Screens/waiting.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class WaitingScreen extends StatelessWidget {
+  const WaitingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('waiting'),
+        ),
+      ),
+    );
+  }
+}
