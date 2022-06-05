2062dc5f1ae61511e2772f454cd637e54980afaa
realnihal <puramnihal@gmail.com>
2022-06-05 22:26:19 +0530

    more efficiency improvement

--- lib/Home Screens/home.dart
+++ lib/Home Screens/home.dart
@@ -49,7 +49,7 @@ class _HomePageState extends State<HomePage> {
 
   Future get_image() async {
     final image =
-        await picker.getImage(source: ImageSource.camera, imageQuality: 1);
+        await picker.getImage(source: ImageSource.camera, imageQuality: 5);
     if (image != null) {
       setState(() {
         _waiting = true;
